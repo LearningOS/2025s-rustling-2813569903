195797658d65dac35e9a38ad2eeb87c8435e5d34
chu <103038397+2813569903@users.noreply.github.com>
2025-04-13 14:12:38 +0000

    update

--- exercises/error_handling/errors4.rs
+++ exercises/error_handling/errors4.rs
@@ -16,8 +16,11 @@ enum CreationError {
 
 impl PositiveNonzeroInteger {
     fn new(value: i64) -> Result<PositiveNonzeroInteger, CreationError> {
-        // Hmm...? Why is this only returning an Ok value?
-        Ok(PositiveNonzeroInteger(value as u64))
+        match value {
+            x if x > 0 => Ok(PositiveNonzeroInteger(value as u64)),
+            x if x < 0 => Err(CreationError::Negative),
+            _ => Err(CreationError::Zero),
+        }
     }
 }
 
